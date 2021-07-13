0480a5c59142a156cf082d262587e6269d36ff5f
Vaze Legend <vaze.legend@gmail.com>
2021-07-13 15:46:31 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="nevamt3sdk",
+    version="0.1.0",
+    description="Custom (self-made) SDK for electricity meters Neva MT 3xx",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Nikita Nemirovsky",
+    author_email="vaze.legend@gmail.com",
+    url="https://github.com/vazelegend/nevamt3sdk-python",
+    license="MIT",
+    package_dir={"": "src"},
+    packages=find_packages("src"),
+    install_requires=["pyserial"],
+    python_requires=">=3.6",
+    # setup_requires=["pytest-runner"],
+    # tests_require=["pytest==6.2.4"],
+    # test_suite="tests",
+)
