63f4ab652c79671befd78513b0f2781c5ff32624
Nikita Nemirovsky <vaze.legend@gmail.com>
2021-07-18 17:21:06 +0300

    Refactoring

--- src/pyneva/__main__.py
+++ src/pyneva/__main__.py
@@ -1,29 +1,25 @@
 from pyneva.meter import Meter
 import argparse
 
-FUNCTION_MAP = ("get_total_energy", "get_phase_voltages", "get_active_power",
-                "get_serial_number", "get_status")
+FUNCTIONS = ("total_energy", "voltage", "amperage", "serial_number", "status")
 
-parser = argparse.ArgumentParser(
-    description="CLI version does not support write mode!\n")
+parser = argparse.ArgumentParser(description="CLI version does not support write mode!\n")
 
 parser.add_argument("--obis", nargs='*', default="", help="Specify OBIS codes")
-parser.add_argument("--func",
-                    nargs='*',
-                    default="",
-                    choices=FUNCTION_MAP,
-                    help="Specify prepared functions")
-parser.add_argument("-p",
-                    "--port",
-                    required=True,
-                    help="Specify serial port interface")
+parser.add_argument("--func", nargs='*', default="", choices=FUNCTIONS, help="Specify prepared functions")
+parser.add_argument("-p", "--port", required=True, help="Specify serial port interface")
 
 args = parser.parse_args()
 
 if len(args.obis) == 0 and len(args.func) == 0:
     parser.error("at least one of --obis and --func required")
 
-with Meter(args.port) as session:
+try:
+    session = Meter(args.port)
+except ConnectionError as e:
+    parser.error(str(e))
+
+with session:
     print(f"Connected to: {session}")
 
     if len(args.obis) != 0:
@@ -35,4 +31,4 @@ with Meter(args.port) as session:
     if len(args.func) != 0:
         print("\nFunctions:")
         for func in args.func:
-            print(f"{func}\t", getattr(session, func)())
+            print(f"{func}\t", getattr(session, func))
--- src/pyneva/meter.py
+++ src/pyneva/meter.py
@@ -1,18 +1,21 @@
-from typing import Dict, Literal, Tuple, Union
+from collections import namedtuple
+from typing import Literal, Tuple, Union
 import serial
 import re
 
 
 class Meter:
     """
-    SDK for working with electricity meters Neva MT 3xx via serial port.
+    Custom library for working with electricity meters Neva MT 3xx via serial port.
     """
 
     password = b""
-    serial_num = ""
+    _serial_num = ""
     device_name = ""
-    working_baudrate = 9600
     init_baudrate = 300
+    working_baudrate = 9600
+    init_timeout = 1
+    working_timeout = 1
 
     def __init__(self, port):
         self.port = port
@@ -21,14 +24,15 @@ class Meter:
             "transfer_request": b"/?!\r\n",
             # <ACK>051<CR><LF>
             "ack": b"\x06051\r\n",
+            "end_conn": b"\x01B0\x03q",
             "total_energy": self.make_request("0F.08.80*FF"),
             "voltage_phase1": self.make_request("20.07.00*FF"),
             "voltage_phase2": self.make_request("34.07.00*FF"),
             "voltage_phase3": self.make_request("48.07.00*FF"),
-            "active_power_phase1": self.make_request("24.07.00*FF"),
-            "active_power_phase2": self.make_request("38.07.00*FF"),
-            "active_power_phase3": self.make_request("4C.07.00*FF"),
-            "active_power_phase_sum": self.make_request("10.07.00*FF"),
+            "amperage_phase1": self.make_request("24.07.00*FF"),
+            "amperage_phase2": self.make_request("38.07.00*FF"),
+            "amperage_phase3": self.make_request("4C.07.00*FF"),
+            "amperage_sum": self.make_request("10.07.00*FF"),
             "serial_num": self.make_request("60.01.00*FF"),
             "status": self.make_request("60.05.00*FF"),
         }
@@ -39,11 +43,12 @@ class Meter:
         Starting serial session, sending initial commands.
         """
 
-        self.session = serial.Serial(self.port,
-                                     self.init_baudrate,
-                                     serial.SEVENBITS,
-                                     serial.PARITY_EVEN,
-                                     timeout=2)
+        self.session = serial.Serial(port=self.port,
+                                     baudrate=self.init_baudrate,
+                                     bytesize=serial.SEVENBITS,
+                                     parity=serial.PARITY_EVEN,
+                                     stopbits=serial.STOPBITS_ONE,
+                                     timeout=self.init_timeout)
 
         self.send_request(self.commands["transfer_request"])
 
@@ -62,7 +67,7 @@ class Meter:
         try:
             self.password = self.parse_response(self.get_response()).encode()
         except ValueError:
-            raise ConnectionError("Password message format is wrong") from None
+            raise ConnectionError("Password message format is wrong, try again") from None
 
         self.send_request(self.make_request(mode="P", data=self.password))
 
@@ -70,15 +75,24 @@ class Meter:
         if msg != b"\x06":
             raise ConnectionError(f"Message is not ACK, message: {msg}")
 
-    def get_total_energy(self) -> tuple[float]:
+        # Change timeout
+        self.session.timeout = self.working_timeout
+
+    TotalEnergy = namedtuple("TotalEnergy", "sum T1 T2 T3 T4")
+
+    @property
+    def total_energy(self) -> TotalEnergy:
         """
         Returns accumulated active energy.
         """
 
         self.send_request(self.commands["total_energy"])
-        return self.parse_response(self.get_response())
+        return self.TotalEnergy(*self.parse_response(self.get_response()))
 
-    def get_phase_voltages(self) -> tuple[float]:
+    Voltage = namedtuple("Voltage", "phase1 phase2 phase3")
+
+    @property
+    def voltage(self) -> Voltage:
         """
         Returns current voltage of all phases.
         """
@@ -92,63 +106,60 @@ class Meter:
         self.send_request(self.commands["voltage_phase3"])
         v3 = self.parse_response(self.get_response())
 
-        return v1, v2, v3
+        return self.Voltage(phase1=v1, phase2=v2, phase3=v3)
 
-    def get_active_power(self) -> tuple[float]:
+    Amperage = namedtuple("Amperage", "sum phase1 phase2 phase3")
+
+    @property
+    def amperage(self) -> Amperage:
         """
         Returns current active power (in watts) of all phases.
         """
 
-        self.send_request(self.commands["active_power_phase_sum"])
+        self.send_request(self.commands["amperage_sum"])
         w_sum = self.parse_response(self.get_response())
 
-        self.send_request(self.commands["active_power_phase1"])
+        self.send_request(self.commands["amperage_phase1"])
         w1 = self.parse_response(self.get_response())
 
-        self.send_request(self.commands["active_power_phase2"])
+        self.send_request(self.commands["amperage_phase2"])
         w2 = self.parse_response(self.get_response())
 
-        self.send_request(self.commands["active_power_phase3"])
+        self.send_request(self.commands["amperage_phase3"])
         w3 = self.parse_response(self.get_response())
 
-        return w_sum, w1, w2, w3
+        return self.Amperage(sum=w_sum, phase1=w1, phase2=w2, phase3=w3)
 
-    def get_serial_number(self) -> int:
-        if self.serial_num:
-            return self.serial_num
+    @property
+    def serial_number(self) -> int:
+        if self._serial_num:
+            return self._serial_num
         self.send_request(self.commands["serial_num"])
-        self.serial_num = self.parse_response(self.get_response())
-        return self.serial_num
+        self._serial_num = self.parse_response(self.get_response())
+        return self._serial_num
 
-    def get_status(self) -> Dict[str, bool]:
+    Status = namedtuple("Status", (
+        "data_memory_err",
+        "param_memory_err",
+        "measurement_err",
+        "clock_err",
+        "battery_discharged",
+        "programming_button_pressed",
+        "data_memory_doesnt_work",
+        "param_memory_doesnt_work",
+        "screw_terminal_cover_removed",
+        "load_connected",
+        "load_disconnected"))
+
+    @property
+    def status(self) -> Status:
         self.send_request(self.commands["status"])
         response = self.parse_response(self.get_response())
         bits = f'{int(response, 16):0>16b}'
         bits = bits[:8] + bits[9:12]
-        status = {
-            "data_memory_err": 0,
-            "param_memory_err": 0,
-            "measurement_err": 0,
-            "clock_err": 0,
-            "battery_discharged": 0,
-            "programming_button_pressed": 0,
-            "data_memory_doesnt_work": 0,
-            "param_memory_doesnt_work": 0,
-            "screw_terminal_cover_removed": 0,
-            "load_connected": 0,
-            "load_disconnected": 0
-        }
-        keys = list(status)
-        for k, v in enumerate(bits):
-            status[keys[k]] = v == "1"
+        return self.Status(*map(lambda x: x == "1", bits))
 
-        return status
-
-    def make_request(self,
-                     obis: str = "",
-                     mode: Literal["P", "W", "R"] = "R",
-                     data: bytes = b"") -> bytes:
-
+    def make_request(self, obis: str = "", mode: Literal["P", "W", "R"] = "R", data: bytes = b"") -> bytes:
         if type(obis) != str:
             raise TypeError(f"OBIS must be str, not {type(obis).__name__}")
 
@@ -175,15 +186,12 @@ class Meter:
 
         return request
 
-    def parse_response(
-            self,
-            response: bytes) -> Union[str, float, Tuple[Union[str, float]]]:
+    def parse_response(self, response: bytes) -> Union[str, float, Tuple[Union[str, float]]]:
         try:
             bracket_idx = response.index(b"(")
             response = response[bracket_idx + 1:-3]
         except (IndexError, ValueError):
-            raise ValueError(
-                f"Invalid response format, response: {response}") from None
+            raise ValueError(f"Invalid response format, response: {response}") from None
 
         response = response.split(b",")
         if b"." in response[0]:
@@ -193,6 +201,12 @@ class Meter:
         if len(response) != 1:
             return tuple(map(lambda x: x.decode("ascii"), response))
         return response[0].decode("ascii")
+
+    def calculate_bcc(self, data: bytes) -> bytes:
+        bcc = 0
+        for byte in data:
+            bcc ^= byte
+        return bcc.to_bytes(1, "little")
 
     def send_request(self, request: bytes):
         self.session.write(request)
@@ -206,17 +220,18 @@ class Meter:
     def connect(self):
         self.session.open()
 
-    def calculate_bcc(self, data: bytes) -> bytes:
-        bcc = 0
-        for byte in data:
-            bcc ^= byte
-        return bcc.to_bytes(1, "little")
+    def close_session(self):
+        self.send_request(self.commands["end_conn"])
+        self.disconnect()
 
     def __str__(self) -> str:
         return self.device_name
+
+    def __repr__(self) -> str:
+        return f"Meter({self.port!r})"
 
     def __enter__(self):
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        self.disconnect()
+        self.close_session()
